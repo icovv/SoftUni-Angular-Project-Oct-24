e2869b5baa803d8d2b1f97d348e4dd9e5a89279f
icovv <hvasilev00@abv.bg>
2024-11-30 16:35:42 +0200

    creating profile component

--- softuni-angular-project/src/app/app.routes.ts
+++ softuni-angular-project/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ListYourCarComponent } from './list-your-car/list-your-car.component';
 import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "/home", pathMatch: "full"},
@@ -10,5 +11,7 @@ export const routes: Routes = [
 
     {path: 'catalog', component: CatalogComponent},
     {path: 'list', component: ListYourCarComponent},
-    {path: 'search', component: SearchComponent}
+    {path: 'search', component: SearchComponent},
+    
+    {path: 'profile', component: ProfileComponent}
 ];
