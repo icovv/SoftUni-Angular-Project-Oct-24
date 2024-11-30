43a33faf2f12994a8c9ae1a9e313688bb573affa
icovv <hvasilev00@abv.bg>
2024-11-30 15:14:34 +0200

    creating components and folder structure

---
+++ softuni-angular-project/src/app/list-your-car/list-your-car.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-your-car',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-your-car.component.html',
+  styleUrl: './list-your-car.component.css'
+})
+export class ListYourCarComponent {
+
+}
