1023de325d136d9471b3764b4704f63c6f3460fd
icovv <hvasilev00@abv.bg>
2024-11-30 16:18:23 +0200

    creating search and list components

--- softuni-angular-project/src/app/app.routes.ts
+++ softuni-angular-project/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { ListYourCarComponent } from './list-your-car/list-your-car.component';
+import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "/home", pathMatch: "full"},
     {path: "home", component: HomeComponent},
 
     {path: 'catalog', component: CatalogComponent},
+    {path: 'list', component: ListYourCarComponent},
+    {path: 'search', component: SearchComponent}
 ];
