1035ce45fbdc6efcf8dd1498bc8197d9c879a6a7
icovv <hvasilev00@abv.bg>
2024-11-30 16:39:07 +0200

    creating contacts component

--- softuni-angular-project/src/app/app.routes.ts
+++ softuni-angular-project/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { ListYourCarComponent } from './list-your-car/list-your-car.component';
 import { SearchComponent } from './search/search.component';
 import { ProfileComponent } from './user/profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "/home", pathMatch: "full"},
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: 'catalog', component: CatalogComponent},
     {path: 'list', component: ListYourCarComponent},
     {path: 'search', component: SearchComponent},
+    {path: 'contacts', component:ContactComponent},
     
     {path: 'profile', component: ProfileComponent}
 ];
