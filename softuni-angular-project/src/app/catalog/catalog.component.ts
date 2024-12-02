fc336bd33ddf885e99a1a075332d702bffd72d0a
icovv <hvasilev00@abv.bg>
2024-12-02 21:31:12 +0200

    implementing routerLink

--- softuni-angular-project/src/app/catalog/catalog.component.ts
+++ softuni-angular-project/src/app/catalog/catalog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
