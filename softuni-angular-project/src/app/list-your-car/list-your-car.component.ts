912d617d85a7408f410c64a5b52b513ecc59504b
icovv <hvasilev00@abv.bg>
2024-12-03 19:39:39 +0200

    creating reactive form in ListYourCarComponent

--- softuni-angular-project/src/app/list-your-car/list-your-car.component.ts
+++ softuni-angular-project/src/app/list-your-car/list-your-car.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list-your-car',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './list-your-car.component.html',
   styleUrl: './list-your-car.component.css'
 })
 export class ListYourCarComponent {
+  form = new FormGroup({
+    brand: new FormControl("",[Validators.required]),
+    year: new FormControl("",[Validators.required]),
+    model: new FormControl("",[Validators.required]),
+    capacity: new FormControl("",[Validators.required]),
+    power: new FormControl("",[Validators.required]),
+    fuel: new FormControl("",[Validators.required]),
+    color: new FormControl("",[Validators.required]),
+    image: new FormControl("",[Validators.required]),
+    description: new FormControl("",[Validators.required]),
+  })
 
+  submit(){
+
+    
+  }
 }
