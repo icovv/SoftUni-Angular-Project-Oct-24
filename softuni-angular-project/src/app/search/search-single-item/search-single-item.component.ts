63d4e266528789982b2192b5e2061c729dd0ff07
icovv <hvasilev00@abv.bg>
2024-12-06 18:16:53 +0200

    creating SearchSingleItemComponent and adding dinamic id on Details button for this component and the CatalogSingleItemComponent

---
+++ softuni-angular-project/src/app/search/search-single-item/search-single-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Cars } from '../../types/cars';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-search-single-item',
+  imports: [RouterLink],
+  templateUrl: './search-single-item.component.html',
+  styleUrl: './search-single-item.component.css'
+})
+export class SearchSingleItemComponent {
+  @Input() car:Cars | null = null;
+  @Input() loggedIn:boolean = false;
+}
--- softuni-angular-project/src/app/search/search.component.ts
+++ softuni-angular-project/src/app/search/search.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { searchValidator } from '../utils/search.validator';
 import { ErrorsComponent } from '../core/errors/errors.component';
+import { ApiService } from '../services/api.service';
+import { UserService } from '../services/user.service';
+import { Cars } from '../types/cars';
+import { SearchSingleItemComponent } from './search-single-item/search-single-item.component';
+import { LoaderComponent } from '../core/loader/loader.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, ErrorsComponent],
+  imports: [ReactiveFormsModule, ErrorsComponent, SearchSingleItemComponent, LoaderComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
   errorContainer:string[] = [];
   form = new FormGroup({
     search: new FormControl('',[]),
@@ -19,6 +24,34 @@ export class SearchComponent {
   },{
     validators:searchValidator()
   })
+
+  get loading(): boolean{
+    return this.cars.length <= 0 ? true : false
+  }
+
+  cars:Cars[] = [];
+  
+  get isLoggedIn():boolean{
+    return this.userService.isLogged;
+  }
+
+  constructor(private api: ApiService, private userService: UserService){
+
+  }
+
+  ngOnInit(): void {
+    this.api.getAllCars().subscribe({
+      next: (data) => {
+        this.cars = data;
+        console.log(this.cars)
+      },
+      error: (err) => {
+        console.log(`this is your err ${err}`)
+        this.errorContainer?.push(err);
+      }
+    })
+  }
+
 
   onAnimationEnd(data:boolean){
     if(data){
